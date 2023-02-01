22c1a1e3bf28595178aa72ebb974e3cd1f918296
eleline <hoangtuanh.d1@gmail.com>
2023-02-01 16:58:28 +0700

    feat: Initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-text-field v-model="msg" />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('Hello World!');
+</script>
